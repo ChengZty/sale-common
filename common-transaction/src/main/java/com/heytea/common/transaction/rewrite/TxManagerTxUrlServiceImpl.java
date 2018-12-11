3a5157efb423d1995ece3aea5daf4e5007a02f90
陈湘辉 <cxh231196@163.com>
2018-12-11 20:28:28 +0800

    lcn

---
+++ common-transaction/src/main/java/com/heytea/common/transaction/rewrite/TxManagerTxUrlServiceImpl.java
@@ -0,0 +1,32 @@
+package com.heytea.common.transaction.rewrite;
+
+import com.codingapi.tx.config.service.TxManagerTxUrlService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.stereotype.Service;
+
+/**
+ * 使用服务发现重写 Txmanager 获取规则
+ */
+@Slf4j
+@Service
+@AllArgsConstructor
+public class TxManagerTxUrlServiceImpl implements TxManagerTxUrlService {
+	private final LoadBalancerClient loadBalancerClient;
+
+	@Override
+	public String getTxUrl() {
+		ServiceInstance serviceInstance = loadBalancerClient.choose("tx-manager");
+		if (serviceInstance == null){
+			log.error("集群中没有  tx-manager服务; 将无法保证分布式事务安全。。。。。。");
+			return null;
+		}
+		String host = serviceInstance.getHost();
+		Integer port = serviceInstance.getPort();
+		String url = String.format("http://%s:%s/tx/manager/", host, port);
+		log.info("tm.manager.url -> {}", url);
+		return url;
+	}
+}
---
+++ common-transaction/src/main/java/com/heytea/common/transaction/tx/springcloud/interceptor/TxManagerInterceptor.java
@@ -0,0 +1,36 @@
+package com.heytea.common.transaction.tx.springcloud.interceptor;
+
+import com.codingapi.tx.aop.service.AspectBeforeService;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ *@author LCN on 2017/6/7.
+ *
+ * @author LCN
+ * @since 4.1.0
+ */
+
+@Component
+public class TxManagerInterceptor {
+
+	@Autowired
+	private AspectBeforeService aspectBeforeService;
+
+	public Object around(ProceedingJoinPoint point) throws Throwable {
+		String groupId = null;
+		try {
+			RequestAttributes requestAttributes = RequestContextHolder.currentRequestAttributes();
+			HttpServletRequest request = ((ServletRequestAttributes) requestAttributes).getRequest();
+			groupId = request.getHeader("tx-group");
+		} catch (Exception e) {
+		}
+		return aspectBeforeService.around(groupId, point);
+	}
+}
